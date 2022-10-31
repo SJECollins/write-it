4481ae93984e22ee5b95761c1ca6b69e836baac9
SCollins <sje.collins@gmail.com>
2022-10-31 10:22:36 +0000

    Pre-deploy

--- writeit/settings.py
+++ writeit/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
@@ -21,14 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+development = os.environ.get('DEVELOPMENT', False)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = []
+if development:
+    ALLOWED_HOSTS = ['localhost']
+else:
+    ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -89,7 +94,8 @@ WSGI_APPLICATION = 'writeit.wsgi.application'
 AUTH_USER_MODEL = 'accounts.account'
 
 AUTHENTICATION_BACKENDS = (
-    ('django.contrib.auth.backends.AllowAllUsersModelBackend'),
+    'django.contrib.auth.backends.ModelBackend',
+    'django.contrib.auth.backends.AllowAllUsersModelBackend',
 )
 
 SITE_ID = 1
@@ -103,12 +109,17 @@ CSRF_TRUSTED_ORIGINS = ['https://8000-sjecollins-writeit-tati0vt5z41.ws-eu73.git
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if development:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 # Password validation
